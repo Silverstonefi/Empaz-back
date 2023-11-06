dffcd4951646ff39e948e1cbaa88fa9544cf6eb3
TaSv <tarerie00annie@gmail.com>
2023-11-06 17:26:32 +0100

    If it works, don't touch it

--- db/conn.js
+++ db/conn.js
@@ -1,12 +1,22 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Connected");
 
-  // console.log(`MongoDB Connected ${conn.connection.host}`);
+  } catch (error) {
+  console.log(`MongoDB Connected ` + error.message);
+
+  }
+  // const conn = await mongoose.connect(process.env.MONGO_URI, {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true,
+  // });
+
 };
 
 export default connectDB;
